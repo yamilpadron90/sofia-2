e62991b5f627f7f1cebe3c0fe33f908b2158f90e
admravisgitea <yrodrigp@everis.com>
2021-04-07 12:30:26 +0200

    st

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'sofia2';
+
+  getStorage() {
+    const a = sessionStorage.getItem("sofia2-example")
+    console.log("Result from SessionStorage", a)
+  }
+
+  setStorage() {
+    sessionStorage.setItem("sofia2-example", "test")
+  }
+
 }
